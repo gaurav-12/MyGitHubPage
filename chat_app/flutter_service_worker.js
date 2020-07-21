e5823f165da31c8e2e096c46896fe7a5f290d82c
Gaurav <gaurav.12apr@gmail.com>
2020-07-22 04:41:50 +0530

    Updated Chat App, fixed minor UI issues for Mobile

--- chat_app/flutter_service_worker.js
+++ chat_app/flutter_service_worker.js
@@ -13,7 +13,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "23904f23b39fc4f20e54563b49d97a3c",
 "index.html": "3357c060ae3d6a46aa836b9cb47b416e",
 "/": "3357c060ae3d6a46aa836b9cb47b416e",
-"main.dart.js": "010f6236750a569d9cdc5da745477504",
+"main.dart.js": "a226dd498c1439ec621aceaad0615ac8",
 "manifest.json": "bb7accda1de65ec19c10f4132ef5d04e"
 };
 
